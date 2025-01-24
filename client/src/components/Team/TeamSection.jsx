85d6d6c485f49d0039e90eefd015698be59b70ed
kadithya-16 <kanduriadithya16@gmail.com>
2025-01-24 23:16:47 +0530

    about page

--- client/src/components/Team/TeamSection.jsx
+++ client/src/components/Team/TeamSection.jsx
@@ -1,65 +1,81 @@
 import React from "react";
-import TeamCard from "../Team/TeamCard";
+// import TeamCard from "../Team/TeamCard";
 import "../../styles/TeamSection.css";
-import image from "../../assets/pothole.jpg";
+import img from "../../assets/pothole.jpg";
 
 function TeamSection() {
-    const teamMembers = [
-        {
-            image: image,
-            name: "Cara Shrivastava",
-            title: "Content Strategist",
-        },
-        {
-            image: image,
-            name: "Chayto Nadin",
-            title: "Designer",
-        },
-        {
-            image: image,
-            name: "Kerstin Thai",
-            title: "UX Designer",
-        },
-        {
-            image: image,
-            name: "Karen Ireson",
-            title: "Junior Strategist",
-        },
-        {
-            image: image,
-            name: "Ben Levitt",
-            title: "Studio & Production Coordinator",
-        },
-        {
-            image: image,
-            name: "Ben Levitt",
-            title: "Studio & Production Coordinator",
-        },
-    ];
-
+    const Member = ({ image, name, description, iconClass1, iconClass2, iconClass3 }) => (
+        <div className="member-card">
+            <div className="member-image">
+                <img src={image} alt="image" />
+            </div>
+            <h3>{name}</h3>
+            <p>{description}</p>
+            <div className="member-icon">
+                <a href="#" target="_blank"><i className={iconClass1}></i></a>
+                <a href="#" target="_blank"><i className={iconClass2}></i></a>
+                <a href="#" target="_blank"><i className={iconClass3}></i></a>
+            </div>
+        </div>
+    );
     return (
-        <section>
-            <h2 className="team-title">Our Team</h2>
-            <div className="team-body">
-                <div className="team-content">
-                    <p className="team-description">
-                        Designers, developers, strategists, producers, specialists & storytellers.
-                    </p>
-                    <div className="team-section">
-                        <div className="team-grid">
-                            {teamMembers.map((member, index) => (
-                                <TeamCard
-                                    key={index}
-                                    image={member.image}
-                                    name={member.name}
-                                    title={member.title}
-                                />
-                            ))}
-                        </div>
+        <>
+            <section >
+                <div className="members" id="members">
+                    <h2>Our Team</h2>
+                    <div className="members-grid">
+                        <Member
+                            image={img}
+                            name="18"
+                            description="Advanced AI algorithms for instant pothole detection"
+                            iconClass1="fa-brands fa-instagram icon"
+                            iconClass2="fa-brands fa-github icon"
+                            iconClass3="fa-brands fa-linkedin icon"
+                        />
+                        <Member
+                            image={img}
+                            name="65"
+                            description="Precise location tracking and mapping of road conditions"
+                            iconClass1="fa-brands fa-instagram icon"
+                            iconClass2="fa-brands fa-github icon"
+                            iconClass3="fa-brands fa-linkedin icon"
+                        />
+                        <Member
+                            image={img}
+                            name="91"
+                            description="Comprehensive data analysis and reporting"
+                            iconClass1="fa-brands fa-instagram icon"
+                            iconClass2="fa-brands fa-github icon"
+                            iconClass3="fa-brands fa-linkedin icon"
+                        />
+                        <Member
+                            image={img}
+                            name="121"
+                            description="Advanced AI algorithms for instant pothole detection"
+                            iconClass1="fa-brands fa-instagram icon"
+                            iconClass2="fa-brands fa-github icon"
+                            iconClass3="fa-brands fa-linkedin icon"
+                        />
+                        <Member
+                            image={img}
+                            name="122"
+                            description="Precise location tracking and mapping of road conditions"
+                            iconClass1="fa-brands fa-instagram icon"
+                            iconClass2="fa-brands fa-github icon"
+                            iconClass3="fa-brands fa-linkedin icon"
+                        />
+                        <Member
+                            image={img}
+                            name="124"
+                            description="Comprehensive data analysis and reporting"
+                            iconClass1="fa-brands fa-instagram icon"
+                            iconClass2="fa-brands fa-github icon"
+                            iconClass3="fa-brands fa-linkedin icon"
+                        />
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </>
     );
 }
 
